d4731eec0441a7e33b2457b110c83370cbfc5523
icchy-san <icchy.tech@gmail.com>
2025-12-06 03:52:13 +0900

    feat: Switch client-side protobuf generation to Connect-ES, integrate Connect client for API communication, and update pnpm nodeLinker.

--- native/app/index.tsx
+++ native/app/index.tsx
@@ -1,6 +1,22 @@
 import { Text, View, StyleSheet } from "react-native";
+import { createClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+import { BookstoreService } from "@/gen/bookstore/v1/bookstore_pb";
+import { useEffect } from "react";
 
 const HomeScreen = () => {
+    const client = createClient(BookstoreService, createConnectTransport({
+        baseUrl: "http://localhost:8080",
+    }));
+
+    useEffect(() => {
+        client.listBooks({
+            shelf: "shelf1",
+        }).then((response) => {
+            console.log(response);
+        });
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text>Home</Text>
